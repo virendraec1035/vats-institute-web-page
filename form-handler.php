5b5dc7a8ab2da5450e46f9b2d3088039bcdba359
virendra kumar <80846491+virendraec1035@users.noreply.github.com>
2024-02-21 00:28:27 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,28 @@
+<?php 
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+
+$email_from='vats@easytutorialspoint.com';
+
+$email_subject='New From Submission';
+
+$email_body="User Name:$name.\n".
+            "User Email:$visitor_email.\n".
+            "Subject:$subject.\n".
+            "User Message:$message.\n";
+ 
+$to = 'virendraec1035@gmail.com';
+
+$headers = "From: $email_from\r\n";
+
+$header.= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location:contact.html");
+
+
+?>
